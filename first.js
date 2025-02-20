d37715e495749089319497a40fd2e1bdb1de17a3
kesavan <kesavanbalaji55@gmail.com>
2025-02-20 07:43:53 -0800

    save-

---
+++ first.js
@@ -0,0 +1,44 @@
+// var a;
+// a=10;
+// var a;
+// a=20;
+// var a=30;
+// let b;
+// b=5;
+// const c=20;
+
+// console.log(Number.parseInt('3.19'));
+// console.log(Number.parseFloat('3.19'));
+
+// // console.log(b--,b);
+// console.log(b/=3);
+// var a=10;
+// var v=' 10';
+// console.log(a!==v);
+
+
+// switch (a===v){
+//     case true:
+//         console.log('Hi');
+//         break;
+//     case false:
+//         console.log('Hello');
+//         break;
+// }
+
+// var a='string one'
+// var b='string one'
+
+// var c=new String('String Object one')
+// var d=new String('String Object two')
+// console.log(a==b);
+// console.log(a===b);
+// console.log(c==d);
+// console.log(c===d);
+
+// var string= new String('Hello World');
+// var ew= string.slice(-2); 
+// console.log(ew);
+
+// console.log(string.padStart(21,'hi'));
+// console.log(string.valueOf());
