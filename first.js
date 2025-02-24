b70c7fde46156031aee8f498923034ef787b205c
kesavan <kesavanbalaji55@gmail.com>
2025-02-24 05:47:07 -0800

    save

--- first.js
+++ first.js
@@ -42,3 +42,137 @@
 
 // console.log(string.padStart(21,'hi'));
 // console.log(string.valueOf());
+
+// var x=10
+// console.log(x--);
+// console.log(--x);
+
+// var x=10
+// if (x%3==0){
+//     console.log('Not Divisible by 2');
+    
+// }else if(x%2==0){
+//     console.log('Divisible by 2');
+    
+// }else{
+//     console.log("Suck");
+    
+// }
+
+// var x=11
+// switch (x%2){
+//     case 0:
+//         console.log('Divisible by 2');
+//         break
+//     case 1:
+//         console.log('Not divisible by 0');
+//         break
+// }
+
+// var a=Number.parseInt(prompt('Enter one number :'));
+// var b=Number.parseInt(prompt('Enter another number :'));
+// alert(a+b);
+
+// function helloBro(){
+//     console.log("Named Function");
+// }
+
+// helloBro()
+
+
+// var a=function(){
+    // console.log("Anonymuis Function");
+// }
+
+// a()
+
+
+// let x=function(){
+//     console.log('Hey bro');
+    
+// }
+// x()
+
+
+// var a=10;
+// var b=20;
+// let x=(a,b)=>{
+//     console.log(a+b);
+// }
+// x(a,b)
+
+
+// var a=30;
+// let z=()=>{
+//     b=20;
+//     let o=(a,b)=>{
+//         console.log(a-b);
+//     }
+//     return a-b;
+// }
+// console.log(z());
+
+// function prime(num){
+//     count=0;
+//     for(i=1;i<=num;i++){
+//         if (num%i==0){
+//             count++;
+//         }
+//     }
+//     if (count==2){
+//         console.log(`${num} is prime`);
+        
+//     }
+//     else{
+//         console.log(`${num} is not a prime number`);
+        
+//     }
+// }
+// prime(3)
+
+
+// function hi(name,msg)
+// {
+// // console.log(` ${name} , ${msg}`)
+// let combinedMsg = name.concat(" ",msg)
+// return combinedMsg
+// }
+// let res = hi("MSD","thala for a reason")
+// console.log(res)
+
+
+// console.log('---------------------------------------------')
+// let add2 = (a,b)=>{
+// console.log(a+b)
+// }
+// let sub3 = (a,b)=>{
+//     console.log(a-b)
+//     }
+//     let mul2 = (a,b)=>{
+//     console.log(a*b)
+//     }
+//     let operate = (a,b,myFunc)=>{
+//     myFunc(a,b)
+//     }
+//     operate(30,10,add2)
+//     operate(10,4,mul2)
+//     operate(20,10,sub3);
+//     console.log('---------------------------------------------------');
+
+
+// (function()
+// {
+// console.log('hello , i am iife')
+// })();
+
+// (function func()
+// {
+// console.log('hello , i am iife')
+// })();
+
+
+var a=Number.parseInt(prompt("Enter a Number :"));
+var b=Number.parseInt(prompt(`Enter another Nuumber :`));
+console.log(a+b);
+
+console.log('Hi');
